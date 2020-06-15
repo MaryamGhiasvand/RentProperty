11923feacac6797577e1d787084f9311d9eeb813
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-06-15 17:48:58 -0500

    add ajax

--- FinalExamJPASec/src/main/java/com/packt/webstore/controller/FavoritePropertiesController.java
+++ FinalExamJPASec/src/main/java/com/packt/webstore/controller/FavoritePropertiesController.java
@@ -3,34 +3,46 @@ package com.packt.webstore.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.packt.webstore.domain.Credentials;
+import com.packt.webstore.domain.FavoriteProperties;
 import com.packt.webstore.domain.Property;
+import com.packt.webstore.service.CredentialService;
 import com.packt.webstore.service.FavoritePropertiesService;
 import com.packt.webstore.service.PropertyService;
 
-
-@Controller
+@RestController
 //@RequestMapping("/FavoriteProperties")
 public class FavoritePropertiesController {
 	@Autowired
 	private FavoritePropertiesService favoritePropertiesService;
-	@Autowired 
+	@Autowired
 	private PropertyService propertyService;
+	@Autowired
+	private CredentialService credentialService;
+
 	
 	@RequestMapping(value = "/addtoFavorite", method = RequestMethod.GET)
-		public  void addtoFavorite(@RequestParam("propertyId") long propertyId) {
+	@ResponseBody 
+		public void addtoFavorite( @RequestParam("propertyId") long propertyId) {
 		System.out.println("**********propertyId*********"+propertyId);
-		 String auth = SecurityContextHolder.getContext().getAuthentication().getName().toString();
-	       System.out.println("**********auth*********"+auth);
+		 String username = SecurityContextHolder.getContext().getAuthentication().getName().toString();
+	       System.out.println("**********auth*********"+username);
 	       
 	       Property p =propertyService.fingPropertyById(propertyId);
-//	       Credentials user = 
-//	       FavoriteProperties favorite = new FavoriteProperties(p,);
-//	       favoritePropertiesService.addToFavorite(favorite);
-		//return "propertyReview";
+	       System.out.println("**********p*********"+p.toString());
+	       Credentials user = credentialService.findByUsername(username);
+	       System.out.println("**********u*********"+user.toString());
+           FavoriteProperties favorite = new FavoriteProperties(p,user);
+	       favoritePropertiesService.addToFavorite(favorite);
+	       System.out.println("**********u*********"+user.toString());
+	       //return "Sucees";
 	}
 
 }
--- FinalExamJPASec/src/main/webapp/resources/js/ajax.js
+++ FinalExamJPASec/src/main/webapp/resources/js/ajax.js
@@ -1,22 +1,25 @@
 var contextRoot = "/" + window.location.pathname.split( '/' )[1];
 
 function addToFavorite(){
-	alert("ajax3 function");
-	alert(contextRoot);
+
 	$.ajax({
 		url:'addtoFavorite',
 		type:'GET',
 		async:false,
 		dataType:'json',
-		data: 'propertyId='+112,
-		success:function(){
+		data: 'propertyId='+111,
+		success:function(sucess){
 			alert("sucess")
+			alert(sucess)
 		    $('#overlay').css('height', $(document.body).height() + 'px')
 		    $('#overlay').show()
 		    $('#dialog').html("<p> successfully added to your favorite list </p>")
 		    centerMe('#dialog')
 		    $('#dialog').show();
-		}
+		},
+	error:function(){
+		alert("error")
+	}
 	
 			
 	})
