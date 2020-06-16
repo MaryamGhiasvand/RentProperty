f949c2bfa959dcce10dc4a7cb7ac1988d5eeba41
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-06-15 19:05:50 -0500

    add ajax

--- FinalExamJPASec/src/main/java/com/packt/webstore/controller/FavoritePropertiesController.java
+++ FinalExamJPASec/src/main/java/com/packt/webstore/controller/FavoritePropertiesController.java
@@ -32,16 +32,12 @@ public class FavoritePropertiesController {
 	@ResponseBody 
 		public String addtoFavorite( @RequestParam("propertyId") long propertyId) {
 		System.out.println("**********propertyId*********"+propertyId);
-		 String username = SecurityContextHolder.getContext().getAuthentication().getName().toString();
-	       System.out.println("**********auth*********"+username);
-	       
+		 String username = SecurityContextHolder.getContext().getAuthentication().getName().toString();	 
+		 //if(username)
 	       Property p =propertyService.fingPropertyById(propertyId);
-	       System.out.println("**********p*********"+p.toString());
 	       Credentials user = credentialService.findByUsername(username);
-	       System.out.println("**********u*********"+user.toString());
            FavoriteProperties favorite = new FavoriteProperties(p,user);
 	       favoritePropertiesService.addToFavorite(favorite);
-	       System.out.println("**********u*********"+user.toString());
 	       return "";
 	}
 
--- FinalExamJPASec/src/main/webapp/resources/js/ajax.js
+++ FinalExamJPASec/src/main/webapp/resources/js/ajax.js
@@ -8,18 +8,18 @@ function addToFavorite() {
 		async : false,
 		dataType : 'html',
 		data : 'propertyId=' + 111,
-		success : function(sucess) {
-			alert("sucess")
-			alert(sucess)
-			$('#overlay').css('height', $(document.body).height() + 'px')
-			$('#overlay').show()
-			$('#dialog').html(
-					"<p> successfully added to your favorite list </p>")
-			centerMe('#dialog')
-			$('#dialog').show();
+		success: function(){
+			$('#errors').html("");
+ 			//$("#result").append( '<H2 align="center" color="green"> Sucessfully addded to your favorite list </H2>');   
+ 			$("#result").append( '<H2> Sucessfully addded to your favorite list </H2>');    
+	 	    $('#result').show();
 		},
 		error: function(xhr, status, error) {
-		    alert(error.message);
+			$('#result').html("");
+			//$("#errors").append( '<H3 align="center"> '+error.message+ '<H3>');  
+			$("#errors").append( '<H2 align="center"> '+"An error occurred, please try again later"+ '</H2>');  
+		  //  alert(error.message);
+			$('#errors').show();
 		 }
 
 	})
